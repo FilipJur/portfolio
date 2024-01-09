6dee3ea8b1c9f06a69d107821bf0bc6f257f92d2
Filip Juran <filipjuranoe@gmail.com>
2024-01-09 02:04:03 +0100

    Vercel adapter

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-vercel';
+
 
 
 export default defineConfig({
+	kit: {
+		adapter: adapter({
+			// see below for options that can be set here
+		})
+	},
 	plugins: [
 		
 		sveltekit()]
