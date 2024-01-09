ba0e29539371551433b351d89b76f8b3d8c0d9f8
Filip Juran <filipjuranoe@gmail.com>
2024-01-09 01:47:32 +0100

    update dependecies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import autoImport from "sveltekit-autoimport"
+
 
 export default defineConfig({
 	plugins: [
-		autoImport({
-			components: ["./src/components"],
-		}),
+		
 		sveltekit()]
 });
