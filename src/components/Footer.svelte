f7f64e53bade0ddeccca604cc804bb81e2f41bda
Filip Juran <filipjuranoe@gmail.com>
2024-01-09 02:14:41 +0100

    Add footer links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,22 +1,49 @@
 <footer
 	class="py-20 sm:py-32 bg-black border-t border-solid border-l-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
 >
-	<p class="px-4 py-2 bg-white text-slate-950 font-medium">
-        Connect with me &darr;
-    </p>
-    <div class="flex flex-col gap-4 items-center justify-center">
-        <p><b class="pr-2">Email</b> filipjuranoe@gmail.com</p>
-        <p><b class="pr-2">Github</b>
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <a href="#" class="text-violet-400" target="_blank">
-                FilipJur<sup><span class="text-xs scale-70 pl-0.5"><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i></span></sup>
-            </a>
-        </p>
-        <p><b class="pr-2">LinkedIn</b>
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <a href="#" class="text-violet-400" target="_blank">
-                FilipJur<sup><span class="text-xs scale-70 pl-0.5"><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i></span></sup>
-            </a>
-        </p>
-    </div>
+	<p class="px-4 py-2 bg-white text-slate-950 font-medium">Connect with me &darr;</p>
+	<div class="flex flex-col gap-4 items-center justify-center">
+		<p><b class="pr-2">Email</b> filipjuranoe@gmail.com</p>
+		<p>
+			<b class="pr-2">Github</b>
+			<!-- svelte-ignore a11y-invalid-attribute -->
+			<a href="https://github.com/FilipJur" class="text-violet-400" target="_blank">
+				FilipJur<sup
+					><span class="text-xs scale-70 pl-0.5"
+						><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i></span
+					></sup
+				>
+			</a>
+		</p>
+		<p>
+			<b class="pr-2">LinkedIn</b>
+			<!-- svelte-ignore a11y-invalid-attribute -->
+			<a
+				href="https://www.linkedin.com/in/filip-jur%C3%A1%C5%88-a6b423207/"
+				class="text-violet-400"
+				target="_blank"
+			>
+				FilipJur<sup
+					><span class="text-xs scale-70 pl-0.5"
+						><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i></span
+					></sup
+				>
+			</a>
+		</p>
+		<p>
+			<b class="pr-2">UpWork</b>
+			<!-- svelte-ignore a11y-invalid-attribute -->
+			<a
+				href="https://www.upwork.com/freelancers/~011e2c519a16eb9300"
+				class="text-violet-400"
+				target="_blank"
+			>
+				FilipJur<sup
+					><span class="text-xs scale-70 pl-0.5"
+						><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i></span
+					></sup
+				>
+			</a>
+		</p>
+	</div>
 </footer>
